ff4efadafc577e0950f6e945d718626001b39ddf
stevenhao <stevenhao@gmail.com>
2022-03-27 22:34:01 -0700

    wip

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* global BigInt */
+
 import "./styles.css";
 import { useState } from "react";
 import { useAsync } from "react-use";
@@ -5,7 +7,8 @@ import sshpk from "sshpk";
 import _, { result } from "lodash";
 const DEFAULT_PUBLIC_KEY =
   "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDBN+ISLXgsf3xxG18ZSKAwARj/0mw0x8JGQoWuCcDB5C99bgC5CMIsm/7ZYHye6BdB7GbY3RV/aVuLzm2lh2Q9opPT2AJhWDdeYyLhrIRsexNfnUXZsETxI4M7P5mZXNHAVASG/Q/gu2lb1aPt5oOiRCI7tvitKLOGrUtb0/KToaityX2OJFmEnmH+RM6t2ICwmfObterWjzm+J5k1ydFjSSwkx669U/GWVf56Rruburz/XlDwUm9liVef5iTOH8/rSu82ejamZXoYJFCaSq3nCZRw8mb6xs+zoiYcKiGozlhg6Zbpkexr4i20vPR5d9rQItaZ38cmbk2HwZzpaqUx/t055CpmUQ2N/vfvzr3rUCeG0SkWsew0m8UDB0AU6LYKCQS50kr0KBYEtE+lt46iLf+5XrlBhFj99xqx5qOeSY9Pz8xuu3Ti2ckDKhyMTj9uONSBPVOxRslX8PK35L0lQdM8TOjKBpVAWx4Fyag93QWyPFdUD4kB+HHSo9FgC9vZxtoxPOpTf8GgIzspGVHL+MjW7QmBs+cD48K9k6XMmaSq1AEx1JjeysoO5d9bzTygyHAhyZtZftnaTQ6r8OjUGL+U9J16Ezp1CwxY8tHpIyh2e6HUuVE8CNkeKLf6j2VIgdQd7b+iSPtr3bc43tMYRW9576Qov/t8pP8gEla83w== stevenhao@gmail.com";
-const DEFAULT_SIGNATURE = `-----BEGIN SSH SIGNATURE-----
+const DEFAULT_SIGNATURE = `
+-----BEGIN SSH SIGNATURE-----
 U1NIU0lHAAAAAQAAAhcAAAAHc3NoLXJzYQAAAAMBAAEAAAIBAME34hIteCx/fHEbXxlIoD
 ABGP/SbDTHwkZCha4JwMHkL31uALkIwiyb/tlgfJ7oF0HsZtjdFX9pW4vObaWHZD2ik9PY
 AmFYN15jIuGshGx7E1+dRdmwRPEjgzs/mZlc0cBUBIb9D+C7aVvVo+3mg6JEIju2+K0os4
@@ -16,21 +19,28 @@ DplumR7GviLbS89Hl32tAi1pnfxyZuTYfBnOlqpTH+3TnkKmZRDY3+9+/OvetQJ4bRKRax
 MqHIxOP2441IE9U7FGyVfw8rfkvSVB0zxM6MoGlUBbHgXJqD3dBbI8V1QPiQH4cdKj0WAL
 29nG2jE86lN/waAjOykZUcv4yNbtCYGz5wPjwr2TpcyZpKrUATHUmN7Kyg7l31vNPKDIcC
 HJm1l+2dpNDqvw6NQYv5T0nXoTOnULDFjy0ekjKHZ7odS5UTwI2R4ot/qPZUiB1B3tv6JI
-+2vdtzje0xhFb3nvpCi/+3yk/yASVrzfAAAABGZpbGUAAAAAAAAABnNoYTUxMgAAAhQAAA
-AMcnNhLXNoYTItNTEyAAACAJk4NZP0P8Z4KCj2gPKy6HSaLK39AJHlH0MUkFZE+YqHkFxn
-3OYF1qM2JIICK+xwrRz3H8j+AOOZhul5mdYvfmkyv1mO2xPpSyei2hseJWmUOYExbhJKst
-3z2ttAhIupK+GAwS3SD6n+KqTPT7RW/W6o3ZkYw6YPuwyVJenIAaoQVdzAO789fppG97yL
-+PZWcmWiVCOnowfW2FbCerUNjCOQU8qSgNJcNentFXmjAkBwomcsu4BKyW1h9EDpwndkRN
-xtWrkfJstwK7K1aUX82/PMmpsp19sr154L9NpvEknIPInlGeZfxmcdV9/NCff4rsCDEO7S
-/UDXWpWrbnSz7UqcWOVQC/6Az0bWVsCgHVkRAWvDE750aikuOzYrZNJrAcH10Cga6EZInQ
-9pH0ijwOoUp6xLzxCCzR+AmO5FGXicFSjamEER1FHXTHz1T9/xPbhDws9EdH7CuLOpfpl8
-bXBjYkwmzh4MUhdhuHdydfkGS6l6QUEG/QBWTIdZWrrYfTO2wrGRMPbncj4/EPveLkAoYx
-GW/JxGMGL8kFWSa3B6hJh6XifcRnoQacAORVosub2Sjm1VtLSaeK7jI/ezOKSTKC3hnHHg
-KgqFar/pjaJV6Y6HTy4o15jC6z9a8ObLDSwqhX+zQzFPGpF18C2wAL4L16L1AVh0jgXGw2
-q6/a4c
++2vdtzje0xhFb3nvpCi/+3yk/yASVrzfAAAAD2RvdWJsZWJsaW5kLnh5egAAAAAAAAAGc2
+hhNTEyAAACFAAAAAxyc2Etc2hhMi01MTIAAAIAULXLPl2eIpe5mAnfBiBW7RIn3X2OZscm
+ipwN/1mvBrbKoRx6srE9i/pARQNJNX9fkcNygWkQeEujHeZMXZfsC/bxzs5z3eKMBRkmR8
+kVKLtgwIrHIdhFYD2DWGNsCdJqM/lmcS7i3wf+nsA+dJNf4o5bJh65CTcaRaKwwrPm+yy8
+su7UMhTtSbf6UmJdatoRoufj5mW0h2DMbLYyq/NYEO4mkgYihtIIaCmbDxg9xjBZPccIhR
+cO2MutnBXY0peUN+FgJKocrE9YkrObai5u6oZTuMaIqx6OEtMk0fop0PI9l6LDA37Mk9U5
+ZPtien4+5XA0IHGzMuXKtAVbSYJkFjb6ecs56y3IXu9iMaQ6lTCapF82MJd3LxcsZeBsN/
+M8Tu0b8O3nWE1qoGaTU4okbSqYrR+F5gh2FARmTRWeS1tQSJ4eImuMD1YhCeL3ykzhTJ+A
+OaGzX22pPCwJIooL+imFeU+7zNpkcU95gzpEeHiak3XopzbAxdDFfOlnTyMwzYEqAt/hp6
+uvLlYF7/eslKLFTvR1CXiovcWWtAyrPmz3T9BcUOA7KeCi1zJ7Se2SAsWQ14SMRAMijDgB
+bFoouQ+iTpmeWFJ51ayjLVmQMItV6HNkVmFeDIlidK8zgGZ8PNN2ti6C6y2pl9B/f9DujO
+T4n+bPmHlzC469r7TPBKU=
 -----END SSH SIGNATURE-----
 `;
 const SSH_MAGIC_PREAMBLE = "SSHSIG";
+const SSH_RESERVED = "";
+
+// the numeric form of the message passed into the primitive 
+// corresponds to the openssh signature produced by the following command:
+// echo "E PLURIBUS UNUM; DO NOT SHARE" | ssh-keygen -Y sign -n do_not_share_this_signature@doubleblind.xyz -f ~/.ssh/id_rsa  | pbcopy
+// regex 
+const MAGIC_DOUBLE_BLIND_REGEX = /^1(ff)+003051300d0609608648016503040203050004403710c692cc2c46207b0c6f9369e709afe9fcdbe1f7097370c1fc7a55aeef8dd0aa9d0a084526dbe59eb24eee4a5320c1f053def2e404c5b45ade44f9b56143e9$/;
 
 function equalArrays(ar1, ar2) {
   if (ar1.length !== ar2.length) return false;
@@ -40,12 +50,12 @@ function equalArrays(ar1, ar2) {
 function bytesToString(bytes) {
   if (bytes.__proto__ !== ArrayBuffer.prototype) {
     if(!bytes.buffer) {
-      debugger;
+      // debugger;
     }
     bytes = bytes.buffer;
   }
   if (!bytes || bytes.__proto__ !== ArrayBuffer.prototype) {
-    debugger;
+    // debugger;
   }
   return new TextDecoder("utf-8").decode(bytes);
 }
@@ -56,7 +66,6 @@ function bytesToInt(bytes) {
 function intToBytes(int) {
   return [Math.floor(int / 256 / 256 / 256) % 256, Math.floor(int / 256 / 256) % 256, Math.floor(int / 256) % 256, int % 256];
 }
-
 function stringToBytes(str) {
   return Uint8Array.from(str, (x) => x.charCodeAt(0))
 }
@@ -105,7 +114,7 @@ async function H(str) {
 }
 
 async function getRawMessage(message, namespace, hash_algorithm) {
-  return concatBytes(stringToBytes("SSHSIG"), packSshStrings([namespace, stringToBytes(""), hash_algorithm, await H(stringToBytes(message))]));
+  return concatBytes(stringToBytes(SSH_MAGIC_PREAMBLE), packSshStrings([namespace, stringToBytes(SSH_RESERVED), hash_algorithm, await H(stringToBytes(message))]));
 }
 
 function getRawSignature(signature) {
@@ -141,10 +150,44 @@ function getRawSignature(signature) {
   // decrypt signature https://github.dev/openssh/openssh-portable/blob/4bbe815ba974b4fd89cc3fc3e3ef1be847a0befe/ssh-rsa.c#L223-L224
   const rawSigParts = unpackSshBytes(rawSignatureEncoded, 2);
   const rawSignature = rawSigParts[1];
+  console.log('raw sigparts is', rawSigParts.map(bytesToString));
 
   console.log("3. bignum bytes", pubKeyParts);
   console.log("4. sig is", rawSignature);
   return [rawSignature, namespace, hash_algorithm, pubKeyEncoded, pubKeyParts];
+}
+
+function modExp(a, b, c) {
+  let res = 1n;
+  for (let i = 0; i < 30; ++i) {
+    if ((b >> i) & 1) res = (res * a) % c;
+    a = a * a % c;
+  }
+  return res;
+}
+
+function bytesToBigInt(bytes) {
+  let res = 0n;
+  for (let i = 0; i < bytes.length; ++i) {
+    res = (res << 8n) + BigInt(bytes[i]);
+  }
+  return res;
+}
+
+
+// circom constants from circuit https://zkrepl.dev/?gist=30d21c7a7285b1b14f608325f172417b
+// template RSAGroupSigVerify(n, k, levels) {
+// component main { public [ root, message ] } = RSAGroupSigVerify(120, 98, 30);
+const CIRCOM_BIGINT_N = 120;
+const CIRCOM_BIGINT_K = 98;
+const CIRCOM_BIGINT_LEVELS = 30;
+function toCircomBigIntBytes(num) {
+  const res = [];
+  const msk = (1n << BigInt(CIRCOM_BIGINT_N)) - 1n;
+  for (let i = 0; i < CIRCOM_BIGINT_K; ++i) {
+    res.push(((num >> BigInt((CIRCOM_BIGINT_K - i - 1) * CIRCOM_BIGINT_N)) & msk).toString());
+  }
+  return res;
 }
 
 // #define MAGIC_PREAMBLE "SSHSIG"
@@ -175,38 +218,35 @@ export default function App() {
       pubKeyEncoded,
       pubKeyParts
     ] = getRawSignature(signature);
-    const rawMessage = await getRawMessage(message, namespace, hash_algorithm);
-    const hashedRawMessage = await H(rawMessage);
     console.log("raw sig is", bytesToString(rawSignature));
-    console.log("raw message is", bytesToString(rawMessage));
     console.log("raw sig bytes is", rawSignature);
-    console.log("raw message bytes is", rawMessage);
     console.log('valid is...' );
-    debugger;
-
-    const verifier = rsaKey.createVerify();
-    verifier.update(rawMessage);
-    debugger;
     const valid = equalArrays(pubKeyParts[2], rsaKey.parts[1].data);
-    console.log('valid is' ,valid);
+    const modulusBigInt = bytesToBigInt(pubKeyParts[2]);
+    const signatureBigInt = bytesToBigInt(rawSignature);
+    console.log(signatureBigInt);
+    const messageBigInt = modExp(signatureBigInt, 65537, modulusBigInt);
+    console.log('valid is', valid);
+    console.log('message bigint is', messageBigInt);
+    const validMessage = !!MAGIC_DOUBLE_BLIND_REGEX.exec(messageBigInt.toString(16));
+    console.log('validMessage is', validMessage);
+    
+    // modExp(bytesToBigInt(rawSignature), 65537, bytesToBigInt(data.modulusBytes))
+
     return {
+      // parts: rsaKey.parts,
       valid,
+      validMessage,
       error,
-      rawSigBytes: Array.from(rawSignature),
-      modulusBytes: Array.from(pubKeyParts[2]),
-      hashedRawMessageBytes: Array.from(hashedRawMessage),
-      rawMessageBytes: Array.from(rawMessage),
-      // rsaKey,
-      // groupKeys
-      // signature[k];
-      //  modulus[k];
-      //  message[k];
+      signature: toCircomBigIntBytes(signatureBigInt),
+      modulus: toCircomBigIntBytes(modulusBigInt),
+      message: toCircomBigIntBytes(messageBigInt),
       //  leaf;
       //  root;
       //  pathElements[levels];
       //  pathIndices[levels];
     };
-  }, [rsaKey, message, groupKeys]);
+  }, [rsaKey, signature, message, groupKeys]);
   return (
     <div className="App">
       <h2>Zero Knowledge RSA Group Signature Generator</h2>
