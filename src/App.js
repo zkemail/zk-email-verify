2f0c915be24a74f246375930c088ebb86021936b
stevenhao <stevenhao@gmail.com>
2022-03-28 01:55:23 -0700

    payload public input

--- src/App.js
+++ src/App.js
@@ -298,6 +298,7 @@ export default function App() {
       modulus: toCircomBigIntBytes(modulusBigInt),
       padded_message: toCircomBigIntBytes(messageBigInt),
       base_message: toCircomBigIntBytes(baseMessageBigInt),
+      payload: await H(message),
       leaf,
       pathElements,
       pathIndices,
