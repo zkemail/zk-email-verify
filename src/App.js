ddf67135312804bc582e42e600973159f58ff359
stevenhao <stevenhao@gmail.com>
2022-03-28 21:51:48 -0700

    fix

--- src/App.js
+++ src/App.js
@@ -300,6 +300,7 @@ export default function App() {
       circuitInput: {
         useNullifier: "1",
         modulus: toCircomBigIntBytes(modulusBigInt),
+        signature: toCircomBigIntBytes(signatureBigInt),
         base_message: toCircomBigIntBytes(baseMessageBigInt),
         payload: payloadHashBigInt.toString(),
         pathElements,
