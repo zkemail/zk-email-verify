8701c5d173798866b9ece5f5885a9bf47e73f574
stevenhao <stevenhao@gmail.com>
2022-03-29 22:59:54 -0700

    file

---
+++ src/helpers/groupSignature/encoding.ts
@@ -0,0 +1,25 @@
+import { IGroupSignature, IIdentityRevealer } from "./types";
+
+export function decodeGroupSignature(sigstring: string): IGroupSignature {
+  return JSON.parse(atob(sigstring.trim().split("\n")[1]));
+}
+
+export function encodeGroupSignature(sig: IGroupSignature) {
+  return (
+    "----BEGIN DOUBLEBLIND.XYZ SIGNATURE----\n" +
+    btoa(JSON.stringify(sig)) +
+    "----END DOUBLEBLIND.XYZ SIGNATURE----"
+  );
+}
+
+export function encodeIdentityRevealer(rev: IIdentityRevealer): string {
+  return (
+    "----BEGIN DOUBLEBLIND.XYZ REVEALER----\n" +
+    btoa(JSON.stringify(rev)) +
+    "----END DOUBLEBLIND.XYZ REVEALER----"
+  );
+}
+
+export function decodeIdentityRevealer(revstring: string): IIdentityRevealer {
+  return JSON.parse(atob(revstring.trim().split("\n")[1]));
+}
