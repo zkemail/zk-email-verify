7e4b6e55a51ff6c2ee0d8ceed493bd7bc0e8685e
Aayush G <aayushg@mit.edu>
2022-07-22 18:34:20 -0400

    fixed forge

--- src/contracts/src/emailHandler.sol
+++ src/contracts/src/emailHandler.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "base64-sol/base64.sol";
+import "./base64.sol";
 import "./emailVerifier.sol";
 
 contract VerifiedEmail is ERC721Enumerable, Verifier {
