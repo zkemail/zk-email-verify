feb3f085dfd41d5421dfa348c416ed24247998af
Divide-By-0 <aayushg@mit.edu>
2022-12-06 17:52:46 -0800

    trying to add an e2e test for browserstack. its not working since create react app hates puppeteer. need to eject

--- src/helpers/zkp.ts
+++ src/helpers/zkp.ts
@@ -2,20 +2,20 @@ import { vkey } from "./vkey";
 
 const localforage = require("localforage");
 const snarkjs = require("snarkjs");
-const tar = require('tar-stream')
-const zlib = require('zlib')
+const tar = require("tar-stream");
+const zlib = require("zlib");
 
 const loadURL = "https://zkemail-zkey-chunks.s3.amazonaws.com/";
 
 export async function downloadFromFilename(filename: string, compressed = false) {
   const link = loadURL + filename;
-  const uncompressFilePromises = []
+  const uncompressFilePromises = [];
   try {
     const zkeyResp = await fetch(link, {
       method: "GET",
     });
     const zkeyBuff = await zkeyResp.arrayBuffer();
-    if(!compressed){
+    if (!compressed) {
       await localforage.setItem(filename, zkeyBuff);
     } else {
       await uncompressAndStore(zkeyBuff, filename);
@@ -26,45 +26,45 @@ export async function downloadFromFilename(filename: string, compressed = false)
   }
 }
 
-const zkeyExtension = ".tar.gz"
+const zkeyExtension = ".tar.gz";
 
 // Un-targz the arrayBuffer into the filename without the .tar.gz on the end
 const uncompressAndStore = async function (arrayBuffer: ArrayBuffer, filename: string) {
   console.log(`Started to uncompress ${filename}...!`);
-  const extract = tar.extract() // create a tar extract stream
-  const gunzip = zlib.createGunzip(arrayBuffer) // create a gunzip stream from the array buffer
-  gunzip.pipe(extract) // pipe the gunzip stream into the tar extract stream
+  const extract = tar.extract(); // create a tar extract stream
+  const gunzip = zlib.createGunzip(arrayBuffer); // create a gunzip stream from the array buffer
+  gunzip.pipe(extract); // pipe the gunzip stream into the tar extract stream
 
   // header is the tar header, stream is the content body (might be an empty stream), call next when you are done with this entry
-  extract.on('entry', function(header: any, stream: any, next: Function) {
+  extract.on("entry", function (header: any, stream: any, next: Function) {
     // decompress the entry data
-    const extractedData: any = []
-    stream.on('data', function(chunk: any) {
-      extractedData.push(chunk)
-    })
+    const extractedData: any = [];
+    stream.on("data", function (chunk: any) {
+      extractedData.push(chunk);
+    });
 
     // make sure to call next when the entry is fully processed
-    stream.on('end', function() {
-      next()
+    stream.on("end", function () {
+      next();
 
-      console.assert(filename.endsWith(zkeyExtension), `Filename doesn't end in ${zkeyExtension}`)
+      console.assert(filename.endsWith(zkeyExtension), `Filename doesn't end in ${zkeyExtension}`);
       const rawFilename = filename.replace(/.tar.gz$/, "");
       // save the extracted data to localForage
-      localforage.setItem(rawFilename, extractedData, function(err: Error) {
+      localforage.setItem(rawFilename, extractedData, function (err: Error) {
         if (err) {
-          console.error(`Couldn't extract data from ${filename}:` + err.message)
+          console.error(`Couldn't extract data from ${filename}:` + err.message);
         } else {
-          console.log('Saved extracted file to localForage')
+          console.log(`Saved ${rawFilename} file to localForage`);
         }
-      })
-    })
-  })
+      });
+    });
+  });
 
   // all entries have been processed
-  extract.on('finish', function() {
-    console.log(`Finished extracting ${filename}`)
-  })
-}
+  extract.on("finish", function () {
+    console.log(`Finished extracting ${filename}`);
+  });
+};
 
 const zkeySuffix = ["b", "c", "d", "e", "f", "g", "h", "i", "j", "k"];
 
@@ -74,7 +74,7 @@ export const downloadProofFiles = async function (filename: string) {
     const itemCompressed = await localforage.getItem(`${filename}.zkey${c}${zkeyExtension}`);
     const item = await localforage.getItem(`${filename}.zkey${c}`);
     if (item || itemCompressed) {
-      console.log(`${filename}.zkey${c}${item?"":zkeyExtension} already found in localstorage!`);
+      console.log(`${filename}.zkey${c}${item ? "" : zkeyExtension} already found in localstorage!`);
       continue;
     }
     filePromises.push(downloadFromFilename(`${filename}.zkey${c}${zkeyExtension}`, true));
@@ -89,10 +89,10 @@ export const uncompressProofFiles = async function (filename: string) {
     const targzFilename = `${filename}.zkey${c}${zkeyExtension}`;
     const item = await localforage.getItem(`${filename}.zkey${c}`);
     const itemCompressed = await localforage.getItem(targzFilename);
-    if (!itemCompressed){
-      console.error(`Error downloading file ${targzFilename}`)
+    if (!itemCompressed) {
+      console.error(`Error downloading file ${targzFilename}`);
     } else {
-      console.log(`${filename}.zkey${c}${item?"":zkeyExtension} already found in localstorage!`);
+      console.log(`${filename}.zkey${c}${item ? "" : zkeyExtension} already found in localstorage!`);
       continue;
     }
     filePromises.push(downloadFromFilename(targzFilename));
@@ -105,6 +105,23 @@ export async function generateProof(input: any, filename: string) {
   // TODO: figure out how to generate this s.t. it passes build
   console.log("generating proof for input");
   console.log(input);
+  localforage.iterate(function (value: any, key: string, iterationNumber: any) {
+    // Get the size of the current value in bytes
+    localforage
+      .getItem(key)
+      .then(function (value: any) {
+        // This code runs once the value has been loaded
+        // from the offline store.
+        console.log(value);
+      })
+      .catch(function (err) {
+        // This code runs if there were any errors
+        console.log(err);
+      });
+    var size = value.length;
+    console.log(typeof key, typeof value, typeof iterationNumber);
+    console.log(key, size);
+  });
   const { proof, publicSignals } = await snarkjs.groth16.fullProve(input, `https://zkemail-zkey-chunks.s3.amazonaws.com/${filename}.wasm`, `${filename}.zkey`);
   console.log(`Generated proof ${JSON.stringify(proof)}`);
 
