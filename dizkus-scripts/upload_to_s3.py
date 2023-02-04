af88fb33f349e563e31940b53a945b924a6adbd7
Divide-By-0 <aayushg@mit.edu>
2023-02-04 17:13:04 -0500

    upload raw .gz files as well

--- dizkus-scripts/upload_to_s3.py
+++ dizkus-scripts/upload_to_s3.py
@@ -2,6 +2,7 @@ import boto3
 import os
 import tarfile
 import time
+import gzip
 
 # Set up the client for the AWS S3 service
 s3 = boto3.client('s3')  # Ask Aayush for the access key and secret access key
@@ -27,12 +28,21 @@ for dir in [zkey_dir, wasm_dir]:
     for file in os.listdir(dir):
         # Check if the file matches the pattern
         if file.startswith('email.zkey'):
+            source_file_path = dir + file
             upload_to_s3(file, dir)  # Uncompressed file
 
-            # Create a zip file for the file
+            # Make a .gz file
+            gz_file_name = file + '.gz'
+            gz_file_path = source_file_path + ".gz"
+            f_in = open(source_file_path)
+            f_out = gzip.open(gz_file_path, 'wb')
+            f_out.writelines(f_in)
+            f_out.close()
+            f_in.close()
+
+            # Create a .tar.gz file for the file
             tar_file_name = file + '.tar.gz'
             with tarfile.open(tar_file_name, 'w:gz') as tar_file:
-                source_file_path = dir + file
                 print("Compressing: ", source_file_path)
                 tar_file.add(source_file_path,
                              arcname=os.path.basename(source_file_path))
@@ -40,7 +50,11 @@ for dir in [zkey_dir, wasm_dir]:
             # Upload the zip file to the AWS bucket, overwriting any existing file with the same name
             upload_to_s3(tar_file_name)
 
+            # Upload the zip file to the AWS bucket, overwriting any existing file with the same name
+            upload_to_s3(gz_file_name)
+
             os.remove(tar_file_name)
+            os.remove(gz_file_name)
 
         if file.startswith('vkey.json') or file.startswith('email.wasm'):
             # Upload the zip file to the AWS bucket, overwriting any existing file with the same name
