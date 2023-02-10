4c3874fe562a8723716ba8e12b36b303441adc15
Vivek Bhupatiraju <vivekab@mit.edu>
2023-02-10 17:56:14 -0500

    added constraint count to README

--- dizkus-scripts/1_compile.sh
+++ dizkus-scripts/1_compile.sh
@@ -11,7 +11,7 @@ fi
 echo '****COMPILING CIRCUIT****'
 start=`date +%s`
 set -x
-circom "../circuits/$CIRCUIT_NAME".circom --r1cs --wasm --sym --c --wat --output "$BUILD_DIR"
+circom "../circuits/$CIRCUIT_NAME".circom --r1cs --output "$BUILD_DIR"
 { set +x; } 2>/dev/null
 end=`date +%s`
 echo "DONE ($((end-start))s)"
