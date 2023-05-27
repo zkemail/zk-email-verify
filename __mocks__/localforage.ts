37dcd223becd1677dda0be2c614d7ac2ebd5699a
Saleel <saleel@saleel.xyz>
2023-05-27 13:54:00 +0530

    chore: use jest for testing instead of react-scripts

--- __mocks__/localforage.ts
+++ __mocks__/localforage.ts
@@ -1,5 +1,8 @@
+import fs from 'fs';
+import path from 'path';
+
 const getUncompressedTestFile = (): ArrayBuffer => {
-  const buffer = fs.readFileSync(`${__dirname}/../__fixtures__/compressed-files/uncompressed-value.txt`);
+  const buffer = fs.readFileSync(path.join(__dirname, `../src/__fixtures__/compressed-files/uncompressed-value.txt`));
   return buffer;
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,27 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import viteTsconfigPaths from 'vite-tsconfig-paths';
-import svgrPlugin from 'vite-plugin-svgr';
-import commonjs from 'vite-plugin-commonjs';
-import { NgmiPolyfill } from 'vite-plugin-ngmi-polyfill';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import viteTsconfigPaths from "vite-tsconfig-paths";
+import svgrPlugin from "vite-plugin-svgr";
+import commonjs from "vite-plugin-commonjs";
+import { NgmiPolyfill } from "vite-plugin-ngmi-polyfill";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		react(),
-		viteTsconfigPaths(),
-		svgrPlugin(),
-		commonjs(),
-		NgmiPolyfill(),
-	],
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+    svgrPlugin(),
+    commonjs(),
+    NgmiPolyfill(),
+  ],
+	server: {
+		port: 3000,
+	},
+  test: {
+		globals: true,
+		environment: 'jsdom',
+    setupFiles: './src/setupTests.js',
+    include: "src/**/*.test.*",
+    exclude: "src/contracts/**/*.test.*|src/e2e**.test.*",
+  },
 });
