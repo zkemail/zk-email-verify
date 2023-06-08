47c15547d2abeaf40c32e66b67b2bf9bc62c14e1
Divide-By-0 <aayushg@mit.edu>
2023-06-08 02:42:50 -0400

    removed bad import

--- src/contracts/script/DeployTwitter.s.sol
+++ src/contracts/script/DeployTwitter.s.sol
@@ -4,25 +4,24 @@ import "forge-std/Test.sol";
 import "forge-std/console.sol";
 import "forge-std/Script.sol";
 import "../src/TwitterEmailHandler.sol";
-import "../src/StringUtils.sol";
 import "../src/Groth16VerifierTwitter.sol";
 
 contract Deploy is Script, Test {
-  function getPrivateKey() internal returns (uint256) {
-    try vm.envUint("PRIVATE_KEY") returns (uint256 privateKey) {
-      return privateKey;
-    } catch {
-      // This is the anvil default exposed secret key
-      return 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+    function getPrivateKey() internal returns (uint256) {
+        try vm.envUint("PRIVATE_KEY") returns (uint256 privateKey) {
+            return privateKey;
+        } catch {
+            // This is the anvil default exposed secret key
+            return 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+        }
     }
-  }
 
-  function run() public {
-    uint256 sk = getPrivateKey();
-    vm.startBroadcast(sk);
-    Verifier proofVerifier = new Verifier();
-    MailServer mailServer = new MailServer();
-    VerifiedTwitterEmail testVerifier = new VerifiedTwitterEmail(proofVerifier, mailServer);
-    vm.stopBroadcast();
-  }
+    function run() public {
+        uint256 sk = getPrivateKey();
+        vm.startBroadcast(sk);
+        Verifier proofVerifier = new Verifier();
+        MailServer mailServer = new MailServer();
+        VerifiedTwitterEmail testVerifier = new VerifiedTwitterEmail(proofVerifier, mailServer);
+        vm.stopBroadcast();
+    }
 }
