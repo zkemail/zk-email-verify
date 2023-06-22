717d8a1b1568ec91135f5d8464b3f8c08e1df23d
Saleel <saleel@saleel.xyz>
2023-06-22 16:22:12 +0530

    feat: add input helpers

--- packages/helpers/shaHash.ts
+++ packages/helpers/shaHash.ts
@@ -5,17 +5,17 @@ import { assert, int64toBytes, int8toBytes, mergeUInt8Arrays, Uint8ArrayToHex } 
 // const { subtle } = webcrypto;
 import { Hash } from "./fast-sha256";
 
-export async function shaHash(str: Uint8Array) {
+export function shaHash(str: Uint8Array) {
   return CryptoJS.createHash('sha256').update(str).digest();
 }
 
-export async function partialSha(msg: Uint8Array, msgLen: number): Promise<Uint8Array> {
+export function partialSha(msg: Uint8Array, msgLen: number): Uint8Array {
   const shaGadget = new Hash();
-  return await shaGadget.update(msg, msgLen).cacheState();
+  return shaGadget.update(msg, msgLen).cacheState();
 }
 
 // Puts an end selector, a bunch of 0s, then the length, then fill the rest with 0s.
-export async function sha256Pad(prehash_prepad_m: Uint8Array, maxShaBytes: number): Promise<[Uint8Array, number]> {
+export function sha256Pad(prehash_prepad_m: Uint8Array, maxShaBytes: number): [Uint8Array, number] {
   let length_bits = prehash_prepad_m.length * 8; // bytes to bits
   let length_in_bytes = int64toBytes(length_bits);
   prehash_prepad_m = mergeUInt8Arrays(prehash_prepad_m, int8toBytes(2 ** 7)); // Add the 1 on the end, length 505
