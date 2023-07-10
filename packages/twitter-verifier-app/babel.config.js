83014ab593b34b9fec36828fa6bc213fc1058f21
Saleel <saleel@saleel.xyz>
2023-07-10 11:17:51 +0530

    chore: fix jest config for envs

--- packages/twitter-verifier-app/babel.config.js
+++ packages/twitter-verifier-app/babel.config.js
@@ -1,8 +1,19 @@
 module.exports = {
+  plugins: [
+    function () {
+      return {
+        visitor: {
+          MetaProperty(path) {
+            path.replaceWithSourceString('process')
+          },
+        },
+      }
+    },
+  ],
   presets: [
     ['@babel/preset-env', { targets: { node: 'current' } }],
     '@babel/preset-typescript',
     ['@babel/preset-react', { "runtime": "automatic" }],
-    ['jest']
+    ['jest'],
   ],
 };
