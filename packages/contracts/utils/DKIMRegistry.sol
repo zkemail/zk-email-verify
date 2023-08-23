d41bb957309d58fb59aa7a15a78f5714c55b68ba
Saleel <saleel@saleel.xyz>
2023-08-24 01:00:42 +0400

    chore: add popular domain hash to DKIMRegistry contract

--- packages/contracts/utils/DKIMRegistry.sol
+++ packages/contracts/utils/DKIMRegistry.sol
@@ -2,17 +2,54 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 /**
   A Registry that store the hash(dkim_public_key) for each domain
  */
 contract DKIMREgistry is Ownable {
+    // Use constants for popular domains to save gas on reads
+    uint256 constant GMAIL_HASH =
+        21238126716164910617487233347059218993958564577330259377744533585136010170208;
+
+    uint256 constant HOTMAIL_HASH =
+        2431254542644577945126644490189743659677343436440304264654087065353925216026;
+
+    uint256 constant TWITTER_HASH =
+        5857406240302475676709141738935898448223932090884766940073913110146444539372;
+
+    uint256 constant ETHEREUM_ORG_HASH =
+        1064717399289379939765004128465682276424933518837235377976999291216925329691;
+
+    uint256 constant SKIFF_HASH =
+        7901875575997183258695482461141301358756276811120772965768802311294654527542;
+
     // Mapping from domain name to DKIM public key hash
     mapping(string => bytes32) public dkimPublicKeyHashes;
 
     function getDKIMPublicKeyHash(
         string memory domainName
     ) public view returns (bytes32) {
+        if (Strings.equal(domainName, "gmail.com")) {
+            return bytes32(GMAIL_HASH);
+        }
+
+        if (Strings.equal(domainName, "hotmail.com")) {
+            return bytes32(HOTMAIL_HASH);
+        }
+
+        if (Strings.equal(domainName, "twitter.com")) {
+            return bytes32(TWITTER_HASH);
+        }
+
+        if (Strings.equal(domainName, "ethereum.org")) {
+            return bytes32(ETHEREUM_ORG_HASH);
+        }
+
+        if (Strings.equal(domainName, "skiff.com")) {
+            return bytes32(SKIFF_HASH);
+        }
+
         return dkimPublicKeyHashes[domainName];
     }
 
