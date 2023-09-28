5885ece8a1c0373e77910b41a529b172cbb631bc
Divide-By-0 <aayushg@mit.edu>
2023-09-28 12:00:17 +0200

    upload to default bucket indexed by commit hash and via relative path

--- packages/twitter-verifier-circuits/scripts/upload_to_s3.py
+++ packages/twitter-verifier-circuits/scripts/upload_to_s3.py
@@ -4,18 +4,18 @@ import tarfile
 import time
 import gzip
 import argparse
+import subprocess
 
 # Set up the client for the AWS S3 service
 s3 = boto3.client('s3')  # Ask Aayush for the access key and secret access key
 
 parser = argparse.ArgumentParser(description='Upload files to S3 bucket')
 parser.add_argument('--bucket_name', type=str, default='zkemail-zkey-chunks', help='Name of the S3 bucket')
-parser.add_argument('--dirs', type=str, default='~/Documents/projects/zk-email-verify/chunked_build/email,~/Documents/projects/zk-email-verify/chunked_build/email/email_js', help="Comma seperated directories to upload files from")
+parser.add_argument('--dirs', type=str, default='./chunked_build/email, ./chunked_build/email/email_js', help="Comma seperated directories to upload files from")
 # parser.add_argument('--build_dir', type=str, default='chunked_build', help='Name of the build directory directory with the circuitname/ folder')
 # parser.add_argument('--circuit_name', type=str, default='email', help='Name of the circuit (i.e. the foldername in build_dir/)')
 parser.add_argument('--prefix_to_tar', type=str, default='email.zkey', help='Prefix to match for files in order to compress to a .tar.gz and upload')
 parser.add_argument('--prefix', type=str, default='vkey.json,email.wasm', help='Comma-seperated prefixes to upload without compression')
-parser.add_argument('--bucket_dir', type=str, help='Directory name to store the zkeys under in the S3 bucket')
 args = parser.parse_args()
 bucket_name = args.bucket_name
 # build_dir = args.build_dir
@@ -23,6 +23,9 @@ bucket_name = args.bucket_name
 prefix_to_tar = args.prefix_to_tar
 prefixes = args.prefix.split(',')
 dirs = args.dirs.split(',')
+
+# Get the latest commit hash
+commit_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('utf-8').strip()
 
 # Set the name of the remote directory and the AWS bucket
 # source = '~/Documents/projects/zk-email-verify'
@@ -33,7 +36,7 @@ dirs = args.dirs.split(',')
 def upload_to_s3(filename, dir=""):
     with open(dir + filename, 'rb') as file:
         print("Starting upload...")
-        s3.upload_fileobj(file, bucket_name, args.bucket_dir + "/" + filename, ExtraArgs={
+        s3.upload_fileobj(file, bucket_name, commit_hash + '/' + filename, ExtraArgs={
                           'ACL': 'public-read', 'ContentType': 'binary/octet-stream'})
         print("Done uploading ", filename, "!")
 
