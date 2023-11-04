da4b0ff72c752307195088fa949792048606a0fb
Saleel <saleel@saleel.xyz>
2023-11-04 12:53:58 +0300

    chore: use array for pubkey hash

--- packages/contracts/interfaces/IDKIMRegistry.sol
+++ packages/contracts/interfaces/IDKIMRegistry.sol
@@ -2,7 +2,8 @@
 pragma solidity ^0.8.0;
 
 interface IDKIMRegistry {
-    function getDKIMPublicKeyHash(
-        string memory domainName
-    ) external view returns (bytes32);
+    function isDKIMPublicKeyHashValid(
+        string memory domainName,
+        bytes32 publicKeyHash
+    ) external view returns (bool);
 }
