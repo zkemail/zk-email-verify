da1bd8bb4364edb037af0d6ed5d6a80a6a3d252d
Metachaser24 <elomukoro@gmail.com>
2024-03-27 13:50:30 +0300

    add pgp remove test eml

--- packages/helpers/src/dkim/index.ts
+++ packages/helpers/src/dkim/index.ts
@@ -21,6 +21,18 @@ export async function verifyDKIMSignature(
   tryRevertARCChanges: boolean = true
 ): Promise<DKIMVerificationResult> {
 
+  const emailStr = email.toString();
+
+  const pgpMarkers = [
+    "BEGIN PGP MESSAGE",
+    "BEGIN PGP SIGNED MESSAGE",
+  ];
+
+  const isPGPEncoded = pgpMarkers.some(marker => emailStr.includes(marker));
+
+  if (isPGPEncoded) {
+    throw new Error("PGP encoded emails are not supported.");
+  }
   let dkimResult = await tryVerifyDKIM(email, domain);
 
   if (dkimResult.status.result !== "pass" && tryRevertARCChanges) {
