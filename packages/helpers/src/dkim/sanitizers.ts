a31991558057a007fd3790e5970789011467010e
Saleel <saleel@saleel.xyz>
2024-04-03 01:18:35 +0530

    helpers: add sanitizer insert13Before10

--- packages/helpers/src/dkim/sanitizers.ts
+++ packages/helpers/src/dkim/sanitizers.ts
@@ -10,15 +10,15 @@ function setHeaderValue(email: string, header: string, value: string) {
   return email.replace(getHeaderValue(email, header), value);
 }
 
-
- // Google sets their own Message-ID and put the original one  in X-Google-Original-Message-ID
- // when ARC forwarding
+// Google sets their own Message-ID and put the original one  in X-Google-Original-Message-ID
+// when ARC forwarding
+// TODO: Add test for this
 function revertGoogleMessageId(email: string): string {
   // (Optional check) This only happens when google does ARC
   if (!email.includes("ARC-Authentication-Results")) {
     return email;
   }
-  
+
   const googleReplacedMessageId = getHeaderValue(
     email,
     "X-Google-Original-Message-ID"
@@ -38,11 +38,27 @@ function removeLabels(email: string): string {
   return sanitized;
 }
 
+// Sometimes, newline encodings re-encode \r\n as just \n, so re-insert the \r
+// TODO: Add test for this
+export function insert13Before10(email: string): string {
+  const byteArray = new TextEncoder().encode(email);
 
-const sanitizers = [
-  revertGoogleMessageId,
-  removeLabels,
-];
+  const ret = new Uint8Array(byteArray.length + 1000);
 
+  let j = 0;
+  for (let i = 0; i < byteArray.length; i++) {
+    // Ensure each \n is preceded by a \r
+    if (byteArray[i] === 10 && i > 0 && byteArray[i - 1] !== 13) {
+      ret[j] = 13;
+      j++;
+    }
+    ret[j] = byteArray[i];
+    j++;
+  }
+
+  return Buffer.from(ret.slice(0, j).buffer).toString();
+}
+
+const sanitizers = [revertGoogleMessageId, removeLabels, insert13Before10];
 
 export default sanitizers;
