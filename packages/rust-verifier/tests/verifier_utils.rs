79735031c9525dff283b83ef8d4957735288fe7c
zkFriendly <zkfriendly@proton.me>
2024-07-12 15:05:25 +0200

    test: embedded verifier test

--- packages/rust-verifier/tests/verifier_utils.rs
+++ packages/rust-verifier/tests/verifier_utils.rs
@@ -1,6 +1,10 @@
 #[cfg(test)]
 mod tests {
+    use std::io::BufWriter;
+
     use ark_crypto_primitives::snark::SNARK;
+    use ark_serialize::CanonicalSerialize;
+    use utils::verifier::verify;
     use utils::verifier_utils::{GrothBn, GrothBnProof, GrothBnVkey, JsonDecoder, PublicInputs};
 
     #[test]
@@ -10,6 +14,27 @@ mod tests {
         let public_inputs: PublicInputs<3> =
             PublicInputs::from_json_file("tests/data/proof_of_twitter/public.json");
         let verified = GrothBn::verify(&vkey, &public_inputs, &proof).unwrap();
+        assert!(verified);
+    }
+
+    #[test]
+    fn test_embedded_verifier() {
+        let proof = GrothBnProof::from_json_file("tests/data/proof_of_twitter/proof.json");
+        let public_inputs: PublicInputs<3> =
+            PublicInputs::from_json_file("tests/data/proof_of_twitter/public.json");
+
+        let mut serialized_public_inputs = Vec::new();
+        let mut serialized_proof = Vec::new();
+        {
+            let writer = BufWriter::new(&mut serialized_public_inputs);
+            public_inputs.inputs.serialize_compressed(writer).unwrap();
+
+            let writer = BufWriter::new(&mut serialized_proof);
+            proof.serialize_compressed(writer).unwrap();
+        }
+
+        let verified = verify(&serialized_proof, &serialized_public_inputs);
+
         assert!(verified);
     }
 }
