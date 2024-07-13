d743b0d197e0456539455b9d4ce01a2d8f87af85
shreyas-londhe <shreyas_londhe@outlook.com>
2024-07-13 16:59:22 +0200

    feat: added body masking template

---
+++ packages/circuits/tests/body-masker.test.ts
@@ -0,0 +1,30 @@
+import { wasm as wasm_tester } from "circom_tester";
+import path from "path";
+
+describe("BodyMasker Circuit", () => {
+    let circuit: any;
+
+    beforeAll(async () => {
+        circuit = await wasm_tester(
+            path.join(__dirname, "./test-circuits/body-masker-test.circom"),
+            {
+                recompile: true,
+                include: path.join(__dirname, "../../../node_modules"),
+                output: path.join(__dirname, "./compiled-test-circuits"),
+            }
+        );
+    });
+
+    it("should mask the body correctly", async () => {
+        const input = {
+            body: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            mask: [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+        };
+
+        const witness = await circuit.calculateWitness(input);
+        await circuit.checkConstraints(witness);
+        await circuit.assertOut(witness, {
+            masked_body: [1, 0, 3, 0, 5, 0, 7, 0, 9, 0],
+        });
+    });
+});
