d718290d661e0ec9519a67a1dfa6bf764a9cf322
Sora Suegami <31360991+SoraSuegami@users.noreply.github.com>
2024-08-29 20:35:03 +0900

    Feat/fix select regex reveal (#214)
    * Fix SelectRegexReveal
    
    * Add a new test for the SelectRegexReveal template.

---
+++ packages/circuits/tests/select-regex-reveal.test.ts
@@ -0,0 +1,117 @@
+import { wasm } from "circom_tester";
+import path from "path";
+
+
+describe("Select Regex Reveal", () => {
+    jest.setTimeout(10 * 60 * 1000); // 10 minutes
+
+    let circuit: any;
+
+    beforeAll(async () => {
+        circuit = await wasm(
+            path.join(__dirname, "./test-circuits/select-regex-reveal-test.circom"),
+            {
+                recompile: true,
+                include: path.join(__dirname, "../../../node_modules"),
+            }
+        );
+    });
+
+    it("should reveal the substring with maximum revealed length", async function () {
+        let input = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        const startIndex = Math.floor(Math.random() * 24);
+        const revealed = Array.from("zk email").map(char => char.charCodeAt(0));
+        for (let i = 0; i < revealed.length; i++) {
+            input[startIndex + i] = revealed[i];
+        }
+        const witness = await circuit.calculateWitness({
+            in: input,
+            startIndex: startIndex,
+        });
+        await circuit.checkConstraints(witness);
+        await circuit.assertOut(witness, { out: revealed })
+    });
+
+    it("should reveal the substring with non-maximum revealed length", async function () {
+        let input = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        const startIndex = 30;
+        const revealed = Array.from("zk").map(char => char.charCodeAt(0));
+        for (let i = 0; i < revealed.length; i++) {
+            input[startIndex + i] = revealed[i];
+        }
+        const witness = await circuit.calculateWitness({
+            in: input,
+            startIndex: startIndex,
+        });
+        await circuit.checkConstraints(witness);
+        await circuit.assertOut(witness, { out: revealed.concat([0, 0, 0, 0, 0, 0]) })
+    });
+
+    it("should fail when all zero", async function () {
+        let input = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        const startIndex = Math.floor(Math.random() * 32);
+        try {
+            const witness = await circuit.calculateWitness({
+                in: input,
+                startIndex: startIndex,
+            });
+            await circuit.checkConstraints(witness);
+        } catch (error) {
+            expect((error as Error).message).toMatch("Assert Failed");
+        }
+    });
+
+    it("should fail when startIndex is 0", async function () {
+        let input = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        const startIndex = 1 + Math.floor(Math.random() * 24);
+        const revealed = Array.from("zk email").map(char => char.charCodeAt(0));
+        for (let i = 0; i < revealed.length; i++) {
+            input[startIndex + i] = revealed[i];
+        }
+        try {
+            const witness = await circuit.calculateWitness({
+                in: input,
+                startIndex: startIndex - 1,
+            });
+            await circuit.checkConstraints(witness);
+        } catch (error) {
+            expect((error as Error).message).toMatch("Assert Failed");
+        }
+    });
+
+    it("should fail when startIndex is not before 0", async function () {
+        let input = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        const startIndex = Math.floor(Math.random() * 23);
+        const revealed = Array.from("zk email").map(char => char.charCodeAt(0));
+        for (let i = 0; i < revealed.length; i++) {
+            input[startIndex + i] = revealed[i];
+        }
+        try {
+            const witness = await circuit.calculateWitness({
+                in: input,
+                startIndex: startIndex + 1,
+            });
+            await circuit.checkConstraints(witness);
+        } catch (error) {
+            expect((error as Error).message).toMatch("Assert Failed");
+        }
+    });
+
+    it("should fail when startIndex is larger than max length", async function () {
+        let input = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        const startIndex = Math.floor(Math.random() * 24);
+        const revealed = Array.from("zk email").map(char => char.charCodeAt(0));
+        for (let i = 0; i < revealed.length; i++) {
+            input[startIndex + i] = revealed[i];
+        }
+        try {
+            const witness = await circuit.calculateWitness({
+                in: input,
+                startIndex: 32
+            });
+            await circuit.checkConstraints(witness);
+        } catch (error) {
+            expect((error as Error).message).toMatch("Assert Failed");
+        }
+    });
+});
