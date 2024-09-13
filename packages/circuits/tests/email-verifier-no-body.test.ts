74958f10f4577375c049f9ee4c27f69b1ffdaf8e
shreyas-londhe <shreyas_londhe@outlook.com>
2024-09-13 13:34:35 +0530

    chore: minor change

--- packages/circuits/tests/email-verifier-no-body.test.ts
+++ packages/circuits/tests/email-verifier-no-body.test.ts
@@ -6,7 +6,7 @@ import { generateEmailVerifierInputsFromDKIMResult } from "@zk-email/helpers/src
 import { verifyDKIMSignature } from "@zk-email/helpers/src/dkim";
 
 describe("EmailVerifier : Without body check", () => {
-    jest.setTimeout(15 * 60 * 1000); // 15 minutes
+    jest.setTimeout(10 * 60 * 1000); // 10 minutes
 
     let dkimResult: DKIMVerificationResult;
     let circuit: any;
